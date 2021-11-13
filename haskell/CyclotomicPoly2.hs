ba74ac3c2f97e05af4b364895097e12b171c61a1
Sergey Samoylenko HP <samsergey@yandex.ru>
2021-11-13 16:56:57 +1200

    Done with cyclotomics

--- haskell/CyclotomicPoly2.hs
+++ haskell/CyclotomicPoly2.hs
@@ -1,21 +1,7 @@
 import Data.List
-import Data.Maybe
 import Data.Numbers.Primes (primeFactors)
 
-showPoly p = foldl showMono "" $ zip (reverse p) [0..]
-  where
-    showMono r (c, i) = r ++ case (c, i) of
-      (0, _) -> ""
-      (c, 0) -> show c
-      (1, i) -> " + " ++ "x^" ++ show i
-      (-1, i) -> " - " ++ "x^" ++ show i
-      (c, i) | c < 0 -> " - " ++ show (-c) ++ "*x^" ++ show i
-      (c, i) | c > 0 -> " + " ++ show c ++ "*x^" ++ show i 
-
-------------------------------------------------------------
-
-negateVar p = zipWith (*) p ones
-  where ones = if odd (length p) then cycle [1,-1] else cycle [-1,1]
+negateVar p = zipWith (*) p $ reverse $ take (length p) $ cycle [1,-1]
 
 lift p 1 = p
 lift p n = intercalate (replicate (n-1) 0) (pure <$> p)
@@ -28,33 +14,58 @@ shortDiv p1 (_:p2) = unfoldr go (length p1 - length p2, p1)
     ker = negate <$> p2 ++ repeat 0
 
 primePowerFactors = sortOn fst . map (\x-> (head x, length x)) . group . primeFactors
-    
-
+                     
+-- simple memoization
 cyclotomics :: [[Integer]]
-cyclotomics = [1,-1] : (cyclotomic <$> [1..])
+cyclotomics = cyclotomic <$> [0..]
 
 cyclotomic :: Int -> [Integer]
-cyclotomic 0 = []
-cyclotomic 1 = [1,-1]
-cyclotomic 2 = [1,1]
+cyclotomic 0 = [0]
+cyclotomic 1 = [1, -1]
+cyclotomic 2 = [1, 1]
 cyclotomic n = case primePowerFactors n of
+  -- for n = 2^k
   [(2,h)]       -> 1 : replicate (2 ^ (h-1) - 1) 0 ++ [1]
+  -- for prime n
   [(p,1)]       -> replicate n 1
+  -- for power of prime n
   [(p,m)]       -> lift (cyclotomics !! p) (p^(m-1))
+  -- for n = 2*p and prime p
   [(2,1),(p,1)] -> take (n `div` 2) $ cycle [1,-1]
+  -- for n = 2*m and odd m
   (2,1):_       -> negateVar $ cyclotomics !! (n `div` 2)
+  -- general case
   (p, m):ps     -> let cm = cyclotomics !! (n `div` (p ^ m))
                    in lift (lift cm p `shortDiv` cm) (p^(m-1))
 
+showPoly p = foldl showMono "" $ zip (reverse p) [0..]
+  where
+    showMono r (c, i) = r ++ case (c, i) of
+      (0, _) -> ""
+      (c, 0) -> show c
+      (c, 1) -> " + " ++ "x"
+      (1, i) -> " + " ++ "x^" ++ show i
+      (-1, i) -> " - " ++ "x^" ++ show i
+      (c, i) | c < 0 -> " - " ++ show (-c) ++ "*x^" ++ show i
+      (c, i) | c > 0 -> " + " ++ show c ++ "*x^" ++ show i 
 
 main = do
   mapM_ (print . showPoly . cyclotomic) [1..30]
   putStrLn $ replicate 40 '-'
-  let indexes = take 4 $ ixs $ maximum . map abs <$> cyclotomics
-  mapM_ (\(j, i) -> putStrLn $ show j ++ " appears in CM(" ++ show i ++ ")") $
-    zip [1..] indexes
-
-ixs :: [Integer] -> [Int]
-ixs l = tail $ scanl (+) 0 $ unfoldr f (1, l)
+  
+  mapM_ showLine $ take 9 task2
   where
-    f (i, l) = let (a, b) = span (/= i) l in Just (length a, (i+1, b))
+    showLine (j, i, l) = putStrLn $ concat [ show j
+                                            , " appears in CM(", show i
+                                            , ") of length ", show l ]
+    
+    -- in order to make computations faster we leave onle each 5-th polynomial
+    task2 = (1,1,2) : tail (search 1 $ zip [0,5..] $ skipBy 5 cyclotomics)
+      where
+        search i ((k, p):ps) = if i `notElem` (abs <$> p)
+                               then search i ps
+                               else (i, k, length p) : search (i+1) ((k, p):ps)
+
+skipBy n [] = []
+skipBy n lst = let (x:_, b) = splitAt n lst
+               in x:skipBy n b
