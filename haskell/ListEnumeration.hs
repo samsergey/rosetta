feeecb06f1eca33a663e8540a4df6ec8032de001
Serget B. Samoylenko <samsergey@yandex.ru>
2021-11-18 21:09:21 +1200

    Done with list enumeration

--- haskell/ListEnumeration.hs
+++ haskell/ListEnumeration.hs
@@ -1,16 +1,28 @@
 import Data.List
-import Data.Char
 
-listToString :: [Int] -> String
-listToString = unwords . map show
-
-stringToInt :: String -> Int
-stringToInt s = foldr go 0 . map (\c -> mod (ord c - 48) 13)
+toBase :: Int -> Integer -> [Int]
+toBase b = unfoldr f
   where
-    go r x = 13*r + x
+    f 0 = Nothing
+    f n = let (q, r) = n `divMod` fromIntegral b in Just (fromIntegral r, q)
 
-charToInt c = mod (ord c - 48) 13
+fromBase :: Int -> [Int] -> Integer
+fromBase n lst = foldr (\x r -> fromIntegral n*r + fromIntegral x) 0 lst
 
-intToChar n = 
-  
-listToInt lst = 0
+------------------------------------------------------------
+               
+listToInt :: Int -> [Int] -> Integer
+listToInt b lst = fromBase (b+1) $ concat seq
+  where
+    seq = [ let (q, r) = divMod n b
+            in replicate q 0 ++ [r+1]
+          | n <- lst ]
+
+intToList :: Int -> Integer -> [Int]
+intToList b lst = go 0 $ toBase (b+1) lst
+  where
+    go 0 [] = []
+    go i (0:xs) = go (i+1) xs
+    go i (x:xs) = (i*b + x - 1) : go 0 xs
+
+
