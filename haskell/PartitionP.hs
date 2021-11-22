55d6b930d4bcdafbca96dfb4ae26f2aebab7f6d2
Sergey Samoylenko HP <samsergey@yandex.ru>
2021-11-22 16:28:16 +1200

    Memoization is faster

--- haskell/PartitionP.hs
+++ haskell/PartitionP.hs
@@ -1,5 +1,3 @@
-import Control.Monad
-
 partDiffDiff n = if odd n then (n + 1) `div` 2 else n + 1
 
 partDiff n = if n < 2 then 1 else partDiff (n - 1) + partDiffDiff (n-1)
@@ -11,23 +9,35 @@ partitionsP n
                     , let pd = partDiff i, pd  <= n ]
 
 
-partitionsP' = partitionsP'' <$> [0..]
+data Tree a = Node a (Tree a) (Tree a)
+
+Node a tl tr !!! 0 = a 
+Node a tl tr !!! n =
+   if odd n
+     then tl !!! top
+     else tr !!! (top-1)
+        where top = n `div` 2
+
+instance Functor Tree where
+   fmap f (Node a tl tr) = Node (f a) (fmap f tl) (fmap f tr)
+
+--naturals = Node 0  (fmap ((+1).(*2)) naturals) (fmap ((*2).(+1)) naturals)
+
+-- naturals r n =
+--    Node n
+--      ((naturals $! r2) $! (n+r))
+--      ((naturals $! r2) $! (n+r2))
+--         where r2 = 2*r
+
+partitionsP' = partitionsP'' <$> naturals
 partitionsP'' n
   | n < 2 = 1
-  | otherwise = sum $ zipWith (*) signs ((partitionsP' !!) <$> terms)
+  | otherwise = sum $ zipWith (*) signs ((partitionsP' !!!) <$> terms)
   where
     terms = (n -) <$> takeWhile (<= n) (tail ofsets)
     signs = cycle [1,1,-1,-1]
 
 ofsets = scanl (+) 0 [if even n then n `div` 2 else n | n <- [1..]]
 
-
-
-partitions :: [Integer]
-partitions = 1 : 1 : rest
- where
-   rest = [] -- (scanl (\r (o, t) -> scanl (+) 0 (zipWith o r t)) (repeat 0) (zip signs terms))
-   terms = map (`drop` partitions) ofsets
-   signs = cycle [1,1,-1,-1]
 
 main = print $ partitionsP'' 6666
