55ee68a7b26c273dc9775e0472d5234d162adb82
Sergey Samoylenko HP <samsergey@yandex.ru>
2021-11-22 14:45:18 +1200

    Randoms

---
+++ haskell/PartitionP.hs
@@ -0,0 +1,34 @@
+import Control.Monad
+
+partDiffDiff n = if odd n then (n + 1) `div` 2 else n + 1
+
+partDiff n = if n < 2 then 1 else partDiff (n - 1) + partDiffDiff (n-1)
+
+partitionsP n
+  | n < 2 = 1
+  | otherwise = sum [ partitionsP (n - pd) * if ((i-1) `mod` 4) < 2 then 1 else (-1)
+                    | i <- [1..n]
+                    , let pd = partDiff i, pd  <= n ]
+
+
+partitionsP' = partitionsP'' <$> [0..]
+partitionsP'' n
+  | n < 2 = 1
+  | otherwise = sum $ zipWith (*) signs ((partitionsP' !!) <$> terms)
+  where
+    terms = (n -) <$> takeWhile (<= n) (tail ofsets)
+    signs = cycle [1,1,-1,-1]
+
+ofsets = scanl (+) 0 [if even n then n `div` 2 else n | n <- [1..]]
+
+
+
+
+partitions :: [Integer]
+partitions = 1 : 1 : 2 : rest
+  where
+    rest = (scanl (\r (o, t) -> scanl (+) 0 (zipWith o r t)) (repeat 0) (zip signs terms))
+    terms = [ drop n partitions | n <- ofsets ]
+    signs = cycle [(+),(+),(-),(-)]
+
+main = print $ partitionsP'' 6666
