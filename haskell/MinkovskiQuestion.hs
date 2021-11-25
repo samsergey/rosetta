b18d24d0f6c346001397857ec755a8ed2a1d520b
Serget B. Samoylenko <samsergey@yandex.ru>
2021-11-26 00:53:41 +1200

    Finished Minkovski

--- haskell/MinkovskiQuestion.hs
+++ haskell/MinkovskiQuestion.hs
@@ -1,81 +1,67 @@
-{-# language DeriveFunctor #-}
 import Data.List (unfoldr)
 import Data.Ratio
+import Data.Tree (Tree (..), levels, unfoldTree)
+import Control.Monad.Zip (mzip)
 
---------------------------------------------------------------------------------
--- infinite binary tree
+------------------------------------------------------------
+-- tree utilities
 
-data Tree a = Node (Tree a) a (Tree a)
-  deriving Functor
-
-node (Node _ a _ ) = a
-
--- smart constructor
 mkTree :: (a -> a -> a) -> a -> a -> Tree a
-mkTree f l r = Node (mkTree f l m) m (mkTree f m r)
-  where m = f l r
-
--- searching a number in the tree
-track :: (Num a, Ord a) => Tree a -> a -> [Int]
-track t x = unfoldr go t ++ [1]
+mkTree f a b = unfoldTree go (a, b)
   where
-    go (Node l a r) = case compare x a of
-        LT -> Just (0, l)
-        EQ -> Nothing
-        GT -> Just (1, r)
+    go (a,b) = let m = f a b in (m, [(a,m), (m,b)])
 
--- following the path in the tree
-follow :: Tree a -> [Int] -> a
-follow t lst =  node $ foldl go t $ init lst
-  where
-    go (Node l a r) x = [l, r] !! x
+pathBy :: Ord b => (a -> b) -> Tree a -> b -> [Either a a]
+pathBy f (Node a [l,r]) x =
+  case x `compare` f a of
+    LT -> Left a : pathBy f l x
+    EQ -> [Right a]
+    GT -> Right a : pathBy f r x 
 
--- list of levels of the tree
-levels :: Tree a -> [[a]]
-levels (Node l a r) = [a] : zipWith (++) (levels l) (levels r)
-
---------------------------------------------------------------------------------
--- some type transformations
-
-toFraction :: RealFrac a => a -> (Int, a)
-toFraction = properFraction
-
-fromFraction :: RealFrac a => (Int, a) -> a
-fromFraction (i, f) = fromIntegral i + f
-
-toFloat :: Fractional a =>  (Int, Int) -> a
-toFloat (a, b) = fromIntegral a / fromIntegral b
-
-toRatio (a, b) = a % b
-fromRatio r = (numerator r, denominator r)
+lookupTree :: Ord a => Tree (a, c) -> a -> c
+lookupTree t =
+  snd . either id id . last . pathBy fst t
 
 oddFunc f 0 = 0
 oddFunc f x = signum x * f (abs x)
-
 --------------------------------------------------------------------------------
--- enumeration of all rationals and their mapping with Minkowski ? function
-
-mean (a,b) (c,d) = (a*d + c*b, 2*b*d)
+toRatio (a, b) = a % b
 mediant (a,b) (c,d) = (a + c, b + d)
+mean (a,b) (c,d) = (a*d + c*b, 2*b*d)
 
-farey :: Tree (Ratio Integer)  
 farey = toRatio <$> mkTree mediant (0, 1) (1, 1)
-   
-minkowski :: Tree (Ratio Integer)  
+
+sternBrocot = toRatio <$> mkTree mediant (0, 1) (1, 0)
+
 minkowski = toRatio <$> mkTree mean (0, 1) (1, 1)
-    
+
 --------------------------------------------------------------------------------
--- ?(x) and inverse implemented as mapping between trees
+
+fromFraction (i, f) = fromIntegral i + f
 
 minkowskiQR :: Ratio Integer -> Ratio Integer
-minkowskiQR = fromFraction . fmap transform . toFraction
+minkowskiQR = fromFraction . fmap transform . properFraction
   where
-    transform = oddFunc (follow minkowski . track farey)
+    transform = oddFunc $ lookupTree (mzip farey minkowski)
 
 invMinkowskiQR :: Ratio Integer -> Ratio Integer
-invMinkowskiQR = fromFraction . fmap transform . toFraction
+invMinkowskiQR = fromFraction . fmap transform . properFraction
   where
-    transform = oddFunc (follow farey . track minkowski)
+    transform = oddFunc $ lookupTree (mzip minkowski farey)
+
+{-
+invMinkowskiQR . minkowskiQR
+fromFraction . fmap transform' . properFraction . fromFraction . fmap transform . properFraction
+fromFraction . fmap transform' . fmap transform . properFraction
+fromFraction . fmap (transform' . transform) . properFraction
+fromFraction . fmap (lookupTree (minkowski `mzip` farey) . lookupTree (farey `mzip` minkowski)) . properFraction
+fromFraction . fmap (lookupTree (minkowski `mzip` farey `mzip` farey `mzip` minkowski) . properFraction
+fromFraction . fmap (lookupTree (minkowski `mzip` minkowski) . properFraction
+fromFraction . fmap id . properFraction
+fromFraction . properFraction
+id
+-}
+
 
 --------------------------------------------------------------------------------
 -- ?(x) and inverse implemented via diadic numerals
@@ -83,17 +69,72 @@ invMinkowskiQR = fromFraction . fmap transform . toFraction
 fromDiadic :: (Int, [Int]) -> Double
 fromDiadic = fromFraction . fmap (foldr go 0 . take 55)
   where
-    go x r = (r + fromIntegral x)/2
+    go x r = (fromIntegral x + r)/2
 
 toDiadic :: Double -> (Int, [Int])
-toDiadic = fmap (unfoldr go) . toFraction
+toDiadic = fmap (unfoldr go) . properFraction
   where
     go x = case properFraction (x * 2) of
              (0, 0) -> Nothing
              (i, f) -> Just (i `mod` 2, f)
 
+{-
+toDiadic . fromDiadic = id
+
+fmap (unfoldr go') . properFraction . fromFraction . fmap (foldr go 0) $ (i,f)
+fmap (unfoldr go') . id . fmap (foldr go 0) $ (i,f)
+fmap (unfoldr go') . fmap (foldr go 0) $ (i,f)
+fmap (unfoldr go' . foldr go 0) $ (i,f)
+(i, unfoldr go' . foldr go 0 $ f)
+(i, unfoldr go' . foldr go 0 $ (h:t))
+(i, unfoldr go' . foldr (\x r -> (x + r)/2) 0 $ (h:t))
+(i, unfoldr go' $ (h + foldr (\x r -> (x + r)/2) 0 t) / 2)
+(i, unfoldr go' $ (\r -> (h + r) / 2) . foldr (\x r -> (x + r)/2) 0 t)
+(i, unfoldr (go' . \r -> (h + r) / 2) $ foldr (\x r -> (x + r)/2) 0 t)
+(i, unfoldr (go'' . properFraction . (* 2) . \r -> (h + r) / 2) $ foldr (\x r -> (x + r)/2) 0 t)
+(i, unfoldr (go'' . properFraction . (* 2) . (/ 2) . (h +)) $ foldr (\x r -> (x + r)/2) 0 t)
+(i, unfoldr (go'' . properFraction . (h +)) $ foldr (\x r -> (x + r)/2) 0 t)
+(i, unfoldr (go'' . first (h+) . properFraction) $ foldr (\x r -> (x + r)/2) 0 t)
+(i, unfoldr (\(i', f) -> Just ((h + i') `mod` 2, f) . properFraction) $ foldr (\x r -> (x + r)/2) 0 t)
+(i, (((h + i') `mod` 2) :) . unfoldr go' . foldr (\x r -> (x + r)/2) 0 $ t)
+-}
+
+track :: Ord a => Tree a -> a -> [Int]
+track t = fmap (either (const 0) (const 1)) . pathBy' id t
+
+follow :: Tree a -> [Int] -> a
+follow t = rootLabel . foldl (\t x -> subForest t !! x) t . init
+
+{-
+follow t (track t x) == id
+
+rootLabel . foldl ((!!) . subForest) t . init . fmap (either (const 0) (const 1)) . pathBy id t $ x
+rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . init . fmap (either (const 0) (const 1)) . pathBy id (Node a [l,r]) $ x
+LT rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . init . fmap (either (const 0) (const 1)) . (Left a :) . pathBy id l $ x
+LT rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . init . (0 :) . fmap (either (const 0) (const 1)) . pathBy id l $ x
+LT rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . (0 :) . init . fmap (either (const 0) (const 1)) . pathBy id l x
+LT rootLabel . foldl ((!!) . subForest) l . init . fmap (either (const 0) (const 1)) . pathBy id l x
+
+EQ rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . init . fmap (either (const 0) (const 1)) . pathBy id (Node a [l,r]) $ a
+EQ rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . init . fmap (either (const 0) (const 1)) $ [Right a]
+EQ rootLabel . foldl ((!!) . subForest) (Node a [l,r]) . init $ [1]
+EQ rootLabel . foldl ((!!) . subForest) (Node a [l,r]) $ []
+EQ rootLabel $ Node a [l,r]
+EQ a
+
+pathBy f (Node a [l,r]) x =
+  case x `compare` f a of
+    LT -> Left a : pathBy f l x   
+    EQ -> [Right a]               
+    GT -> Right a : pathBy f r x  
+
+foldl f x0 [] = x0
+foldl f x0 (h:t) = foldl f (f x0 h) t
+
+-}
+
 minkowskiQF :: Double -> Double
-minkowskiQF = oddFunc $ fromDiadic . fmap transform . toFraction
+minkowskiQF = oddFunc $ fromDiadic . fmap transform . properFraction
   where
     transform 0 = []
     transform f = track (fromRational <$> farey) f
@@ -103,12 +144,25 @@ invMinkowskiQF = oddFunc $ fromFraction . fmap transform . toDiadic
   where
     transform [] = 0
     transform f = follow (fromRational <$> farey) f 
-   
+
+{-
+invMinkowskiQF . minkowskiQF
+fromFraction . fmap transform' . toDiadic . fromDiadic . fmap transform . properFraction
+fromFraction . fmap transform' . fmap transform . properFraction
+fromFraction . fmap (transform' . transform) . properFraction
+fromFraction . fmap (follow (fromRational <$> farey) . track (fromRational <$> farey)) . properFraction
+fromFraction . fmap (follow t . track t) . properFraction
+fromFraction . fmap id . properFraction
+fromFraction . properFraction
+id
+-}
+
+    
 --------------------------------------------------------------------------------
 -- tests
 
 -- sequence of all positive rationals
-rationals = take 1000 $ concat (levels sternBrocot)
+rationals = concat (levels sternBrocot)
   where
     sternBrocot = toRatio <$> mkTree mediant (0, 1) (1, 0)
 
@@ -123,11 +177,14 @@ testIds =
   , testEqF (minkowskiQF . invMinkowskiQF) id . fmap fromRational
   , testEq  (minkowskiQF . fromRational) (fromRational . minkowskiQR) ]
 
+
+--------------------------------------------------------------------------------
+
 {-
 
 =={{header|Haskell}}==
 
-=== Using Farey tree ===
+=== Exact rational function using Farey tree ===
 
 In a lazy functional language Minkowski question mark function can be implemented using one of it's basic properties: 
 
@@ -139,66 +196,44 @@ This recursive definition can be implemented as lazy corecursion, i.e. by genera
 
 First we define tools to handle trees.
 
-<lang haskell>{-# language DeriveFunctor #-}
-import Data.List (unfoldr)
+<lang haskell>import Data.List (unfoldr)
 import Data.Ratio
-
-data Tree a = Node (Tree a) a (Tree a)
-  deriving Functor
-
-node (Node _ a _ ) = a
-
--- smart constructor
-mkTree :: (a -> a -> a) -> a -> a -> Tree a
-mkTree f l r = Node (mkTree f l m) m (mkTree f m r)
-  where m = f l r
-
--- path to given element in the tree
-track :: (Num a, Ord a) => Tree a -> a -> [Int]
-track t x = unfoldr go t ++ [1]
-  where
-    go (Node l a r) = case compare x a of
-        LT -> Just (0, l)
-        EQ -> Nothing
-        GT -> Just (1, r)
-
--- following the path in the tree
-follow :: Tree a -> [Int] -> a
-follow t lst =  node $ foldl go t $ init lst
-  where
-    go (Node l a r) x = [l, r] !! x
-
--- list of levels of the tree
-levels :: Tree a -> [[a]]
-levels (Node l a r) = [a] : zipWith (++) (levels l) (levels r)
+import Data.Tree (Tree (..), levels, unfoldTree)
+import Control.Monad.Zip (mzip)
 
 --------------------------------------------------------------------------------
--- some type transformators and helper functions
+-- some type transformations
 
-toFraction :: RealFrac a => a -> (Int, a)
-toFraction = properFraction
-
-fromFraction :: RealFrac a => (Int, a) -> a
 fromFraction (i, f) = fromIntegral i + f
-
-toFloat :: Fractional a =>  (Int, Int) -> a
 toFloat (a, b) = fromIntegral a / fromIntegral b
-
 toRatio (a, b) = a % b
 fromRatio r = (numerator r, denominator r)
 
 oddFunc f 0 = 0
-oddFunc f x = signum x * f (abs x)</lang>
+oddFunc f x = signum x * f (abs x)
+
+------------------------------------------------------------
+-- general tree utilities
+
+mkTree :: (a -> a -> a) -> a -> a -> Tree a
+mkTree f a b = unfoldTree go (a, b)
+  where
+    go (a,b) = let m = f a b in (m, [(a,m), (m,b)])
+
+pathBy :: Ord b => (a -> b) -> Tree a -> b -> [Either a a]
+pathBy f (Node a [l,r]) x =
+  case x `compare` f a of
+    LT -> Left a : pathBy f l x
+    EQ -> [Right a]
+    GT -> Right a : pathBy f r x</lang>
 
 Now it is possible to define two trees:
 
-<lang haskell>mean (a,b) (c,d) = (a*d + c*b, 2*b*d)
-mediant (a,b) (c,d) = (a + c, b + d)
+<lang haskell>mediant (a,b) (c,d) = (a + c, b + d)
+mean (a,b) (c,d) = (a*d + c*b, 2*b*d)
 
-farey :: Tree (Ratio Integer)  
 farey = toRatio <$> mkTree mediant (0, 1) (1, 1)
-   
-minkowski :: Tree (Ratio Integer)  
+
 minkowski = toRatio <$> mkTree mean (0, 1) (1, 1)</lang>
 
 <pre>λ> mapM_ print $ take 4 $ levels farey
@@ -215,15 +250,19 @@ minkowski = toRatio <$> mkTree mean (0, 1) (1, 1)</lang>
 
 Here is symmetric definitions of {{math|?(x)}} and {{math|?<sup>-1</sup>(x)}} for rational numbers:
 
-<lang haskell>minkowskiQR :: Ratio Integer -> Ratio Integer
-minkowskiQR = fromFraction . fmap transform . toFraction
+<lang haskell>lookupTree :: Ord a => Tree (a, c) -> a -> c
+lookupTree t =
+  snd . either id id . last . pathBy fst t
+
+minkowskiQR :: Ratio Integer -> Ratio Integer
+minkowskiQR = fromFraction . fmap transform . properFraction
   where
-    transform = oddFunc (follow minkowski . track farey)
+    transform = oddFunc $ lookupTree (mzip farey minkowski)
 
 invMinkowskiQR :: Ratio Integer -> Ratio Integer
-invMinkowskiQR = fromFraction . fmap transform . toFraction
+invMinkowskiQR = fromFraction . fmap transform . properFraction
   where
-    transform = oddFunc (follow farey . track minkowski)</lang>
+    transform = oddFunc $ lookupTree (mzip minkowski farey)</lang>
 
 <pre>λ> minkowskiQR (1/2)
 1 % 2
@@ -236,7 +275,7 @@ invMinkowskiQR = fromFraction . fmap transform . toFraction
 λ> invMinkowskiQR (13/256)
 5 % 27</pre>
 
-=== Using diadic numerals ===
+=== Floating point function using Farey tree ===
 
 Paths leading to numbers in Farey tree, give diadic representation of corresponding value of Minkowski ?-function and it's argument. So it is possible to use Farey tree to define Minkowski function for floating point numbers.
 
@@ -246,14 +285,22 @@ fromDiadic = fromFraction . fmap (foldr go 0 . take 55)
     go x r = (r + fromIntegral x)/2
 
 toDiadic :: Double -> (Int, [Int])
-toDiadic = fmap (unfoldr go) . toFraction
+toDiadic = fmap (unfoldr go) . properFraction
   where
     go x = case properFraction (x * 2) of
              (0, 0) -> Nothing
              (i, f) -> Just (i `mod` 2, f)
 
+track :: Ord a => Tree a -> a -> [Int]
+track t =
+  fmap (either (const 0) (const 1)) . pathBy id t
+
+follow :: Tree a -> [Int] -> a
+follow t lst =
+  rootLabel $ foldl (\t -> (subForest t !!)) t $ init lst
+
 minkowskiQF :: Double -> Double
-minkowskiQF = oddFunc $ fromDiadic . fmap transform . toFraction
+minkowskiQF = oddFunc $ fromDiadic . fmap transform . properFraction
   where
     transform 0 = []
     transform f = track (fromRational <$> farey) f
@@ -280,9 +327,8 @@ invMinkowskiQF = oddFunc $ fromFraction . fmap transform . toDiadic
 The task and tests:
 
 <lang haskell>-- sequence of all positive rationals
+sternBrocot = toRatio <$> mkTree mediant (0, 1) (1, 0)
 rationals = concat (levels sternBrocot)
-  where
-    sternBrocot = toRatio <$> mkTree mediant (0, 1) (1, 0)
 
 testEq f g  = all (\x -> f x == g x)
 testEqF f g = all (\x -> abs (f x - g x) < 1e-11)
@@ -307,3 +353,4 @@ testIds =
 [True,True,True,True,True]</pre>
 
 -}
+
